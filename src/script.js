cd424de92d77a2d36188f99df8f4f5a01f879a01
Fede <115208276+FedeL-7688@users.noreply.github.com>
2026-06-11 19:07:16 -0300

    initial implementation of Weather API calls
    to do: translate the farenheit temp to Celsius temp

--- src/script.js
+++ src/script.js
@@ -1,1 +1,22 @@
 import './style.css'
+
+function weatherSource() {
+    return new Promise((resolve, reject) => {
+        fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/M5521?key=R69VLPJQYN9VB8XVYC3D9BC66&lang=es")
+            .then(response => response.json())
+            .then(response => {
+                console.log(response);
+                resolve(response)
+            })
+    
+    .catch(error => reject(error))
+})
+}
+
+weatherSource()
+
+
+
+
+
+ const myAPIKey = "R69VLPJQYN9VB8XVYC3D9BC66";
